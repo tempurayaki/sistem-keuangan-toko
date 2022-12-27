1663f95a620c2d39fd0cb1fee135953f241dfcef
alvita <alvitaaadamayantiii@gmail.com>
2022-12-27 22:48:44 +0700

    syg aku

--- sistem-keuangan-toko-uas/FormJenisBarang.vb
+++ sistem-keuangan-toko-uas/FormJenisBarang.vb
@@ -1,3 +1,6 @@
 ﻿Public Class FormJenisBarang
-
+    Private Sub BtnHapusJenisBarang_Click(sender As Object, e As EventArgs) Handles BtnHapusJenisBarang.Click
+        FormBarangMasuk.Show()
+        Me.Close()
+    End Sub
 End Class
