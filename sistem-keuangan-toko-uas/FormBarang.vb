6727664e959236ae5ff29cdd6094c72fa8cccc0b
tempurayaki <sarahumaiira@gmail.com>
2022-12-27 21:54:31 +0700

    test branch

--- sistem-keuangan-toko-uas/FormBarang.vb
+++ sistem-keuangan-toko-uas/FormBarang.vb
@@ -1,3 +1,5 @@
 ﻿Public Class FormBarang
-
+    Private Sub Btn_TambahBarang_Click(sender As Object, e As EventArgs) Handles Btn_TambahBarang.Click
+        MessageBox.Show("wow seru bgt")
+    End Sub
 End Class
