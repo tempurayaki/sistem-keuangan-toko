b3b6cf2bb0ed6423f23ec6f4a41eed34b52d9bef
dhiza <dhizahayati23@gmail.com>
2022-12-28 09:50:51 +0700

    menambahkan form penjualan

--- sistem-keuangan-toko-uas/FormPenjualan.Designer.vb
+++ sistem-keuangan-toko-uas/FormPenjualan.Designer.vb
@@ -42,6 +42,7 @@ Partial Class FormPenjualan
         Me.MenuStrip1 = New System.Windows.Forms.MenuStrip()
         Me.Label8 = New System.Windows.Forms.Label()
         Me.Btn_UpdatePenjualan = New System.Windows.Forms.Button()
+        Me.Label1 = New System.Windows.Forms.Label()
         CType(Me.DGV_Penjualan, System.ComponentModel.ISupportInitialize).BeginInit()
         Me.Panel1.SuspendLayout()
         Me.MenuStrip1.SuspendLayout()
@@ -51,83 +52,78 @@ Partial Class FormPenjualan
         '
         Me.Panel2.BackgroundImage = Global.sistem_keuangan_toko_uas.My.Resources.Resources.b187df37_a063_49e6_8879_162287c6bb6d_removebg_preview
         Me.Panel2.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom
-        Me.Panel2.Location = New System.Drawing.Point(840, 99)
-        Me.Panel2.Margin = New System.Windows.Forms.Padding(4, 5, 4, 5)
+        Me.Panel2.Location = New System.Drawing.Point(588, 59)
         Me.Panel2.Name = "Panel2"
-        Me.Panel2.Size = New System.Drawing.Size(316, 180)
+        Me.Panel2.Size = New System.Drawing.Size(221, 108)
         Me.Panel2.TabIndex = 74
         '
         'Label2
         '
         Me.Label2.AutoSize = True
-        Me.Label2.Location = New System.Drawing.Point(17, 21)
-        Me.Label2.Margin = New System.Windows.Forms.Padding(4, 0, 4, 0)
+        Me.Label2.Location = New System.Drawing.Point(12, 13)
         Me.Label2.Name = "Label2"
-        Me.Label2.Size = New System.Drawing.Size(90, 25)
+        Me.Label2.Size = New System.Drawing.Size(58, 15)
         Me.Label2.TabIndex = 11
         Me.Label2.Text = "ID Barang"
         '
         'Label5
         '
         Me.Label5.AutoSize = True
-        Me.Label5.Location = New System.Drawing.Point(17, 124)
-        Me.Label5.Margin = New System.Windows.Forms.Padding(4, 0, 4, 0)
+        Me.Label5.Location = New System.Drawing.Point(12, 74)
         Me.Label5.Name = "Label5"
-        Me.Label5.Size = New System.Drawing.Size(107, 25)
+        Me.Label5.Size = New System.Drawing.Size(71, 15)
         Me.Label5.TabIndex = 16
         Me.Label5.Text = "Tanggal Jual"
         '
         'Label3
         '
         Me.Label3.AutoSize = True
-        Me.Label3.Location = New System.Drawing.Point(17, 72)
-        Me.Label3.Margin = New System.Windows.Forms.Padding(4, 0, 4, 0)
+        Me.Label3.Location = New System.Drawing.Point(12, 43)
         Me.Label3.Name = "Label3"
-        Me.Label3.Size = New System.Drawing.Size(127, 25)
+        Me.Label3.Size = New System.Drawing.Size(85, 15)
         Me.Label3.TabIndex = 12
         Me.Label3.Text = "Jumlah Barang"
         '
         'Date_Jual
         '
-        Me.Date_Jual.Location = New System.Drawing.Point(210, 124)
-        Me.Date_Jual.Margin = New System.Windows.Forms.Padding(4, 3, 4, 3)
+        Me.Date_Jual.Location = New System.Drawing.Point(147, 74)
+        Me.Date_Jual.Margin = New System.Windows.Forms.Padding(3, 2, 3, 2)
         Me.Date_Jual.Name = "Date_Jual"
-        Me.Date_Jual.Size = New System.Drawing.Size(311, 31)
+        Me.Date_Jual.Size = New System.Drawing.Size(219, 23)
         Me.Date_Jual.TabIndex = 14
         '
         'Txt_ID_Barang
         '
-        Me.Txt_ID_Barang.Location = New System.Drawing.Point(210, 16)
-        Me.Txt_ID_Barang.Margin = New System.Windows.Forms.Padding(4, 3, 4, 3)
+        Me.Txt_ID_Barang.Location = New System.Drawing.Point(147, 10)
+        Me.Txt_ID_Barang.Margin = New System.Windows.Forms.Padding(3, 2, 3, 2)
         Me.Txt_ID_Barang.Name = "Txt_ID_Barang"
-        Me.Txt_ID_Barang.Size = New System.Drawing.Size(311, 31)
+        Me.Txt_ID_Barang.Size = New System.Drawing.Size(219, 23)
         Me.Txt_ID_Barang.TabIndex = 7
         '
         'Txt_JumlahBarang
         '
-        Me.Txt_JumlahBarang.Location = New System.Drawing.Point(210, 72)
-        Me.Txt_JumlahBarang.Margin = New System.Windows.Forms.Padding(4, 3, 4, 3)
+        Me.Txt_JumlahBarang.Location = New System.Drawing.Point(147, 43)
+        Me.Txt_JumlahBarang.Margin = New System.Windows.Forms.Padding(3, 2, 3, 2)
         Me.Txt_JumlahBarang.Name = "Txt_JumlahBarang"
-        Me.Txt_JumlahBarang.Size = New System.Drawing.Size(311, 31)
+        Me.Txt_JumlahBarang.Size = New System.Drawing.Size(219, 23)
         Me.Txt_JumlahBarang.TabIndex = 8
         '
         'Label7
         '
         Me.Label7.AutoSize = True
         Me.Label7.Font = New System.Drawing.Font("Segoe UI", 13.8!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point)
-        Me.Label7.Location = New System.Drawing.Point(474, 56)
-        Me.Label7.Margin = New System.Windows.Forms.Padding(4, 0, 4, 0)
+        Me.Label7.Location = New System.Drawing.Point(332, 34)
         Me.Label7.Name = "Label7"
-        Me.Label7.Size = New System.Drawing.Size(222, 38)
+        Me.Label7.Size = New System.Drawing.Size(151, 25)
         Me.Label7.TabIndex = 69
         Me.Label7.Text = "Form Penjualan"
         '
         'Btn_HapusPenjualan
         '
-        Me.Btn_HapusPenjualan.Location = New System.Drawing.Point(756, 211)
-        Me.Btn_HapusPenjualan.Margin = New System.Windows.Forms.Padding(4, 3, 4, 3)
+        Me.Btn_HapusPenjualan.Location = New System.Drawing.Point(529, 127)
+        Me.Btn_HapusPenjualan.Margin = New System.Windows.Forms.Padding(3, 2, 3, 2)
         Me.Btn_HapusPenjualan.Name = "Btn_HapusPenjualan"
-        Me.Btn_HapusPenjualan.Size = New System.Drawing.Size(184, 42)
+        Me.Btn_HapusPenjualan.Size = New System.Drawing.Size(129, 25)
         Me.Btn_HapusPenjualan.TabIndex = 68
         Me.Btn_HapusPenjualan.Text = "Hapus Penjualan"
         Me.Btn_HapusPenjualan.UseVisualStyleBackColor = True
@@ -135,15 +131,15 @@ Partial Class FormPenjualan
         'SignOutToolStripMenuItem
         '
         Me.SignOutToolStripMenuItem.Name = "SignOutToolStripMenuItem"
-        Me.SignOutToolStripMenuItem.Size = New System.Drawing.Size(346, 40)
+        Me.SignOutToolStripMenuItem.Size = New System.Drawing.Size(224, 26)
         Me.SignOutToolStripMenuItem.Text = "Sign Out"
         '
         'Btn_TambahPenjualan
         '
-        Me.Btn_TambahPenjualan.Location = New System.Drawing.Point(756, 111)
-        Me.Btn_TambahPenjualan.Margin = New System.Windows.Forms.Padding(4, 3, 4, 3)
+        Me.Btn_TambahPenjualan.Location = New System.Drawing.Point(529, 67)
+        Me.Btn_TambahPenjualan.Margin = New System.Windows.Forms.Padding(3, 2, 3, 2)
         Me.Btn_TambahPenjualan.Name = "Btn_TambahPenjualan"
-        Me.Btn_TambahPenjualan.Size = New System.Drawing.Size(184, 42)
+        Me.Btn_TambahPenjualan.Size = New System.Drawing.Size(129, 25)
         Me.Btn_TambahPenjualan.TabIndex = 66
         Me.Btn_TambahPenjualan.Text = "Tambah Penjualan"
         Me.Btn_TambahPenjualan.UseVisualStyleBackColor = True
@@ -152,12 +148,12 @@ Partial Class FormPenjualan
         '
         Me.DGV_Penjualan.BackgroundColor = System.Drawing.SystemColors.MenuBar
         Me.DGV_Penjualan.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize
-        Me.DGV_Penjualan.Location = New System.Drawing.Point(64, 394)
-        Me.DGV_Penjualan.Margin = New System.Windows.Forms.Padding(4, 3, 4, 3)
+        Me.DGV_Penjualan.Location = New System.Drawing.Point(45, 236)
+        Me.DGV_Penjualan.Margin = New System.Windows.Forms.Padding(3, 2, 3, 2)
         Me.DGV_Penjualan.Name = "DGV_Penjualan"
         Me.DGV_Penjualan.RowHeadersWidth = 51
         Me.DGV_Penjualan.RowTemplate.Height = 29
-        Me.DGV_Penjualan.Size = New System.Drawing.Size(1033, 379)
+        Me.DGV_Penjualan.Size = New System.Drawing.Size(723, 227)
         Me.DGV_Penjualan.TabIndex = 65
         '
         'Panel1
@@ -168,28 +164,28 @@ Partial Class FormPenjualan
         Me.Panel1.Controls.Add(Me.Date_Jual)
         Me.Panel1.Controls.Add(Me.Txt_ID_Barang)
         Me.Panel1.Controls.Add(Me.Txt_JumlahBarang)
-        Me.Panel1.Location = New System.Drawing.Point(177, 99)
-        Me.Panel1.Margin = New System.Windows.Forms.Padding(4, 3, 4, 3)
+        Me.Panel1.Location = New System.Drawing.Point(124, 59)
+        Me.Panel1.Margin = New System.Windows.Forms.Padding(3, 2, 3, 2)
         Me.Panel1.Name = "Panel1"
-        Me.Panel1.Size = New System.Drawing.Size(557, 182)
+        Me.Panel1.Size = New System.Drawing.Size(390, 109)
         Me.Panel1.TabIndex = 71
         '
         'TSMenu_FormBarangMasuk
         '
         Me.TSMenu_FormBarangMasuk.Name = "TSMenu_FormBarangMasuk"
-        Me.TSMenu_FormBarangMasuk.Size = New System.Drawing.Size(346, 40)
+        Me.TSMenu_FormBarangMasuk.Size = New System.Drawing.Size(224, 26)
         Me.TSMenu_FormBarangMasuk.Text = "Form Barang Masuk"
         '
         'TSMenu_FormBarang
         '
         Me.TSMenu_FormBarang.Name = "TSMenu_FormBarang"
-        Me.TSMenu_FormBarang.Size = New System.Drawing.Size(346, 40)
+        Me.TSMenu_FormBarang.Size = New System.Drawing.Size(224, 26)
         Me.TSMenu_FormBarang.Text = "Form Barang"
         '
         'TSMenu_FormJenisBarang
         '
         Me.TSMenu_FormJenisBarang.Name = "TSMenu_FormJenisBarang"
-        Me.TSMenu_FormJenisBarang.Size = New System.Drawing.Size(346, 40)
+        Me.TSMenu_FormJenisBarang.Size = New System.Drawing.Size(224, 26)
         Me.TSMenu_FormJenisBarang.Text = "Form Jenis Barang"
         '
         'PilihFormLainnyaToolStripMenuItem
@@ -198,7 +194,7 @@ Partial Class FormPenjualan
         Me.PilihFormLainnyaToolStripMenuItem.DropDownItems.AddRange(New System.Windows.Forms.ToolStripItem() {Me.TSMenu_FormJenisBarang, Me.TSMenu_FormBarang, Me.TSMenu_FormBarangMasuk, Me.SignOutToolStripMenuItem})
         Me.PilihFormLainnyaToolStripMenuItem.Font = New System.Drawing.Font("Segoe UI Semibold", 12.0!, CType((System.Drawing.FontStyle.Bold Or System.Drawing.FontStyle.Italic), System.Drawing.FontStyle), System.Drawing.GraphicsUnit.Point)
         Me.PilihFormLainnyaToolStripMenuItem.Name = "PilihFormLainnyaToolStripMenuItem"
-        Me.PilihFormLainnyaToolStripMenuItem.Size = New System.Drawing.Size(235, 36)
+        Me.PilihFormLainnyaToolStripMenuItem.Size = New System.Drawing.Size(158, 25)
         Me.PilihFormLainnyaToolStripMenuItem.Text = "Pilih Form Lainnya"
         '
         'MenuStrip1
@@ -207,8 +203,8 @@ Partial Class FormPenjualan
         Me.MenuStrip1.Items.AddRange(New System.Windows.Forms.ToolStripItem() {Me.PilihFormLainnyaToolStripMenuItem})
         Me.MenuStrip1.Location = New System.Drawing.Point(0, 0)
         Me.MenuStrip1.Name = "MenuStrip1"
-        Me.MenuStrip1.Padding = New System.Windows.Forms.Padding(7, 3, 0, 3)
-        Me.MenuStrip1.Size = New System.Drawing.Size(1169, 42)
+        Me.MenuStrip1.Padding = New System.Windows.Forms.Padding(5, 2, 0, 2)
+        Me.MenuStrip1.Size = New System.Drawing.Size(818, 29)
         Me.MenuStrip1.TabIndex = 72
         Me.MenuStrip1.Text = "MenuStrip1"
         '
@@ -216,28 +212,37 @@ Partial Class FormPenjualan
         '
         Me.Label8.AutoSize = True
         Me.Label8.Font = New System.Drawing.Font("Segoe UI", 13.0!, CType((System.Drawing.FontStyle.Bold Or System.Drawing.FontStyle.Italic), System.Drawing.FontStyle), System.Drawing.GraphicsUnit.Point)
-        Me.Label8.Location = New System.Drawing.Point(467, 340)
-        Me.Label8.Margin = New System.Windows.Forms.Padding(4, 0, 4, 0)
+        Me.Label8.Location = New System.Drawing.Point(327, 204)
         Me.Label8.Name = "Label8"
-        Me.Label8.Size = New System.Drawing.Size(260, 36)
+        Me.Label8.Size = New System.Drawing.Size(185, 25)
         Me.Label8.TabIndex = 70
         Me.Label8.Text = "Database Penjualan"
         '
         'Btn_UpdatePenjualan
         '
-        Me.Btn_UpdatePenjualan.Location = New System.Drawing.Point(756, 159)
-        Me.Btn_UpdatePenjualan.Margin = New System.Windows.Forms.Padding(4, 3, 4, 3)
+        Me.Btn_UpdatePenjualan.Location = New System.Drawing.Point(529, 95)
+        Me.Btn_UpdatePenjualan.Margin = New System.Windows.Forms.Padding(3, 2, 3, 2)
         Me.Btn_UpdatePenjualan.Name = "Btn_UpdatePenjualan"
-        Me.Btn_UpdatePenjualan.Size = New System.Drawing.Size(184, 45)
+        Me.Btn_UpdatePenjualan.Size = New System.Drawing.Size(129, 27)
         Me.Btn_UpdatePenjualan.TabIndex = 67
         Me.Btn_UpdatePenjualan.Text = "Update Penjualan"
         Me.Btn_UpdatePenjualan.UseVisualStyleBackColor = True
         '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(136, 179)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(41, 15)
+        Me.Label1.TabIndex = 75
+        Me.Label1.Text = "Label1"
+        '
         'FormPenjualan
         '
-        Me.AutoScaleDimensions = New System.Drawing.SizeF(10.0!, 25.0!)
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(7.0!, 15.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.ClientSize = New System.Drawing.Size(1169, 814)
+        Me.ClientSize = New System.Drawing.Size(818, 449)
+        Me.Controls.Add(Me.Label1)
         Me.Controls.Add(Me.Label7)
         Me.Controls.Add(Me.Btn_HapusPenjualan)
         Me.Controls.Add(Me.Btn_TambahPenjualan)
@@ -247,6 +252,7 @@ Partial Class FormPenjualan
         Me.Controls.Add(Me.Label8)
         Me.Controls.Add(Me.Btn_UpdatePenjualan)
         Me.Controls.Add(Me.Panel2)
+        Me.Margin = New System.Windows.Forms.Padding(2, 2, 2, 2)
         Me.Name = "FormPenjualan"
         Me.Text = "Penjualan"
         CType(Me.DGV_Penjualan, System.ComponentModel.ISupportInitialize).EndInit()
@@ -279,4 +285,5 @@ Partial Class FormPenjualan
     Friend WithEvents MenuStrip1 As MenuStrip
     Friend WithEvents Label8 As Label
     Friend WithEvents Btn_UpdatePenjualan As Button
+    Friend WithEvents Label1 As Label
 End Class
--- sistem-keuangan-toko-uas/FormPenjualan.vb
+++ sistem-keuangan-toko-uas/FormPenjualan.vb
@@ -1,3 +1,208 @@
-﻿Public Class FormPenjualan
+﻿Imports System.Reflection.Emit
+Imports System.Text.RegularExpressions
+Imports MySql.Data.MySqlClient
+
+Public Class FormPenjualan
+        Public CheckIDKasir As Boolean 'Cek ID Kasir valid?
+        Public CheckIDBarang As Boolean 'Cek ID Kasir valid?
+        Public CheckJumlahBarang As Boolean 'Cek ID Kasir valid?
+        Public CheckTanggalMasuk As Boolean 'Cek ID Kasir valid?
+
+        Public id_penjualan As Integer
+        Public id_barang As Integer
+
+        'Entah kenapa gak konek dari class barang
+        Public Function strconnection() As MySqlConnection
+            Return New MySqlConnection("server=localhost;username=root;password='';database='sistem-keuangan-toko';Convert Zero Datetime=True")
+        End Function
+        Public strcon As MySqlConnection
+
+        Dim mycmd As New MySqlCommand
+        Dim myconnection As New ClassBarang
+        Dim dadapter As MySqlDataAdapter
+        Dim dTabel As New DataTable
+
+        Sub New()
+
+            ' This call is required by the designer.
+            InitializeComponent()
+            ResultDGV()
+            ' Add any initialization after the InitializeComponent() call.
+
+        End Sub
+
+        Sub CheckID()
+
+            'Mengecek Apabila ID Barang sudah benar formatnya
+            If Not Regex.Match(Txt_ID_Barang.Text, "^[0-9]*$", RegexOptions.IgnoreCase).Success Then 'Only Numbers
+
+                MessageBox.Show("Isi ID_Barang dengan Angka Saja!") 'Inform User
+
+                Txt_ID_Barang.Focus() 'Return Focus
+                Txt_ID_Barang.Clear() 'Clear TextBox
+
+                CheckIDBarang = False 'Boolean = False
+            Else
+
+                CheckIDBarang = True 'Everything Fine
+
+            End If
+
+
+        End Sub
+
+        'Public Sub reloadtext(ByVal sql As String)
+        '    Try
+        '        strcon.Open()
+        '        With mycmd
+        '            .Connection = strcon
+        '            .CommandText = sql
+        '        End With
+        '        dTabel = New DataTable
+        '        dadapter = New MySqlDataAdapter
+        '        dadapter.Fill(dTabel)
+        '    Catch ex As Exception
+        '        MessageBox.Show(ex.Message & "reloadText")
+        '    Finally
+        '        If strcon.State = ConnectionState.Open Then
+        '            strcon.Close()
+        '            dTabel.Dispose()
+        '        End If
+        '    End Try
+        'End Sub
+
+        Sub ResultDGV()
+            mycmd.Connection = myconnection.open
+            mycmd.CommandText = "SELECT * FROM penjualan ORDER BY tanggal_jual"
+            dadapter = New MySqlDataAdapter(mycmd)
+            dTabel.Rows.Clear()
+            dadapter.Fill(dTabel)
+            If Not dTabel Is Nothing AndAlso dTabel.Rows.Count > 0 Then
+                DGV_Penjualan.DataSource = dTabel
+
+                With DGV_Penjualan
+                    .Columns(0).HeaderText = "ID_Penjualan"
+                    .Columns(1).HeaderText = "ID Barang"
+                    .Columns(2).HeaderText = "Jumlah Barang"
+                    .Columns(3).HeaderText = "Tanggal Penjualan"
+                End With
+            End If
+
+            myconnection.close()
+        End Sub
+
+        Private Sub Btn_TambahPenjualan_Click(sender As Object, e As EventArgs) Handles Btn_TambahPenjualan.Click
+            'Mengecek Sudah keisi atau belum
+            Try
+                If Txt_ID_Barang.Text = "" Then
+                    MessageBox.Show("ID Kosong! Mohon diisi sesuai yang ada dalam database", "Pemberitahuan", MessageBoxButtons.OK, MessageBoxIcon.Information)
+                Else
+                    CheckID() 'Ngecek formatnya dulu
+                    'reloadtext("SELECT * FROM penjualan WHERE id_penjualan='" & Txt_ID_Barang.Text & "'")
+                    mycmd.Connection = myconnection.open
+                    mycmd.CommandText = "INSERT INTO penjualan(id_barang, jumlah_barang, tanggal_jual)
+                                            VALUES('" _
+                                            & Txt_ID_Barang.Text & "', '" _
+                                            & Txt_JumlahBarang.Text & "', '" _
+                                            & Date_Jual.Value.ToString("yyyy/MM/dd") & "')"
+                    mycmd.ExecuteNonQuery()
+                    myconnection.close()
+                    ResultDGV()
+                    MsgBox("Data Penjualan tersimpan!", MsgBoxStyle.Information, "Pemberitahuan Tambah Penjualan")
+                    'Else
+                    '    MessageBox.Show("ID tidak tersedia dalam database", "Pemberitahuan", MessageBoxButtons.OK, MessageBoxIcon.Information)
+
+                End If
+            Catch ex As Exception
+
+            End Try
+
+
+        End Sub
+
+        Private Sub Btn_UpdatePenjualan_Click(sender As Object, e As EventArgs) Handles Btn_UpdatePenjualan.Click
+
+            '    'Jangan dibuka
+            '    'Dim selectedBarang As List(Of String) = Barang.GetDataKoleksiByIDDatabase(selectedTableBarang)
+
+            '    ''Dim ID As Integer = selectedBarang(0)
+            '    'Barang.GSNamaBarangProperty = selectedBarang(1)
+            '    'Barang.GSStockProperty = selectedBarang(2)
+            '    'Barang.GShargaBarangProperty = selectedBarang(3)
+            '    'Barang.GStanggalMasukBarangProperty = selectedBarang(4)
+            '    'Barang.GStanggalkadaluarsaBarangProperty = selectedBarang(5)
+
+            CheckID()
+
+            mycmd.Connection = myconnection.open
+            mycmd.CommandText = "UPDATE penjualan SET " &
+                            “id_barang='" & Txt_ID_Barang.Text & "', " &
+                            “jumlah_barang='" & Txt_JumlahBarang.Text & "', " &
+                            “tanggal_jual='" & Date_Jual.Value.ToString("yyyy/MM/dd") & "' " &
+                            "WHERE id_penjualan='" & id_penjualan & "' "
+            mycmd.ExecuteNonQuery()
+            myconnection.close()
+
+            'Barang.UpdateDataBarangByIODatabase(id_barang, Txt_NamaBarang.Text, Txt_stock.Text, Txt_harga.Text, Date_Masuk.Value.ToString("yyyy/MM/dd"), Date_Kadaluarsa.Value.ToString("yyyy/MM/dd"))
+            ResultDGV()
+            MsgBox("Data terupdate !", MsgBoxStyle.Information, "Pemberitahuan")
+        End Sub
+
+
+        '     Update barang_masuk
+        'SET barang_masuk.jumlah_barang_masuk = Txt_ID_Barang.Text + barang.stock
+
+        'From barang_masuk  INNER Join barang On barang_masuk.id_barang_masuk = barang.id_barang
+
+        Private Sub Btn_HapusPenjualan_Click(sender As Object, e As EventArgs) Handles Btn_HapusPenjualan.Click
+            'Dim ID As Integer
+            CheckID()
+
+            mycmd.Connection = myconnection.open
+            mycmd.CommandText = “DELETE FROM penjualan “ &
+                           “WHERE id_penjualan='" & id_penjualan & "'"
+            mycmd.ExecuteNonQuery()
+            myconnection.close()
+            ResultDGV()
+            MsgBox("Data terhapus !", MsgBoxStyle.Information, "Pemberitahuan")
+        End Sub
+
+        Private Sub DGV_Barang_CellClick(sender As Object, e As DataGridViewCellEventArgs) Handles DGV_Penjualan.CellClick
+            Dim DGVIndex As Integer
+            Dim SelectedRow As DataGridViewRow
+            SelectedRow = DGV_Penjualan.Rows(DGVIndex)
+
+            With DGV_Penjualan
+                If e.RowIndex >= 0 Then
+                    DGVIndex = .CurrentRow.Index
+                    Label1.Text = .Rows(DGVIndex).Cells(0).Value.ToString
+                    id_penjualan = .Rows(DGVIndex).Cells(0).Value.ToString
+                    Txt_ID_Barang.Text = .Rows(DGVIndex).Cells(1).Value.ToString
+                    Txt_JumlahBarang.Text = .Rows(DGVIndex).Cells(2).Value.ToString
+                    Date_Jual.Value = .Rows(DGVIndex).Cells(3).Value.ToString
+                End If
+            End With
+        End Sub
+
+        Private Sub TSMenu_FormJenisBarang_Click(sender As Object, e As EventArgs)
+            Dim formJenis = New FormJenisBarang()
+            formJenis.Show()
+            Me.Close()
+        End Sub
+
+
+
+
+        Private Sub SignOutToolStripMenuItem_Click(sender As Object, e As EventArgs)
+            MsgBox("Apakah anda yakin ingin keluar?", MessageBoxButtons.YesNo)
+            Me.Close()
+        End Sub
+
+        Private Sub TSMenu_FormBarangMasuk_Click(sender As Object, e As EventArgs) Handles TSMenu_FormBarangMasuk.Click
+            Dim formbarangmasuk = New FormBarangMasuk()
+            formbarangmasuk.Show()
+            Me.Close()
+        End Sub
+
 
 End Class
