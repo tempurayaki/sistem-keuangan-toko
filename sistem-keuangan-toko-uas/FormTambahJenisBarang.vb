bf61627bd8c323c948dadf8f46bebc7bd1d48906
alvita <alvitaaadamayantiii@gmail.com>
2023-01-18 16:23:14 +0700

    push form jenis barang, class jenis barang

---
+++ sistem-keuangan-toko-uas/FormTambahJenisBarang.vb
@@ -0,0 +1,13 @@
+﻿Public Class FormTambahJenisBarang
+    Private Sub BtnTambahJenisBarang_Click(sender As Object, e As EventArgs) Handles BtnTambahJenisBarang.Click
+        FormJenisBarang.jenisbarang.namaJenisBarangProperty = TxtTambahJenisBarang.Text.ToString()
+
+        FormJenisBarang.jenisbarang.AddDataJenisBarangDatabase(FormJenisBarang.jenisbarang.namaJenisBarangProperty)
+
+        Me.Close()
+    End Sub
+
+    Private Sub ToolStripButton1_Click(sender As Object, e As EventArgs) Handles ToolStripButton1.Click
+        Me.Close()
+    End Sub
+End Class
